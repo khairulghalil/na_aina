46aecc3e6c33357012d954bef61137ab2af99f92
Khairul Ghalil <khairulghalil@Khairul-MacBook.local>
2026-04-26 11:27:54 +0800

    Dev on site

--- main.js
+++ main.js
@@ -34,3 +34,26 @@ if (preloader) {
     preloader.remove();
   });
 }
+
+// Swiper
+function initSwiper() {
+  document.querySelectorAll(".init-swiper").forEach(function (swiperElement) {
+    let config = JSON.parse(
+      swiperElement.querySelector(".swiper-config").innerHTML.trim(),
+    );
+
+    if (swiperElement.classList.contains("swiper-tab")) {
+      initSwiperWithCustomPagination(swiperElement, config);
+    } else {
+      new Swiper(swiperElement, config);
+    }
+  });
+}
+
+window.addEventListener("load", initSwiper);
+
+// Whatsapp Me function
+function whatsappMe() {
+  window.location.href =
+    "https://wa.me/60179062684?text=Assalamualaikum+Cik+Na+";
+}

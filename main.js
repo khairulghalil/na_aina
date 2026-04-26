40a99a4a15468bcc58f84bbc7b4d0a27f99a8ff2
Khairul Ghalil <khairulghalil@Khairul-MacBook.local>
2026-04-26 16:59:04 +0800

    Dev modal

--- main.js
+++ main.js
@@ -58,17 +58,54 @@ function whatsappMe() {
     "https://wa.me/60179062684?text=Assalamualaikum+Cik+Na+";
 }
 
-if (history.scrollRestoration) {
-  history.scrollRestoration = "manual";
+// Modal Product
+const modalData = [
+  {
+    title: "Jamu Bunga Ratu Pari dan Kacip Fatimah",
+    url: "../public/img/testImg.png",
+    description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+  },
+  {
+    title: "Jamu Tongkat Ali dan Ubi Jaga",
+    url: "../public/img/testImg.png",
+    description: `Ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+  },
+  {
+    title: "Jamu Cengkih dan Kulit Kayu Manis",
+    url: "../public/img/testImg.png",
+    description: `Dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`,
+  },
+];
+function openModal(index) {
+  const data = modalData[index];
+
+  document.getElementById("dynamicTitle").innerText = data.title;
+  document.getElementById("dynamicImg").src = data.url;
+  document.getElementById("dynamicDesc").innerText = data.description;
+
+  const myModal = new bootstrap.Modal(document.getElementById("productModal"));
+  myModal.show();
 }
 
-window.addEventListener("load", () => {
-  if (window.location.hash) {
-    history.replaceState(
-      "",
-      document.title,
-      window.location.pathname + window.location.search,
-    );
-  }
-  window.scrollTo(0, 0);
-});
+// document
+//   .getElementById("productModal")
+//   .addEventListener("hidden.bs.modal", () => {
+//     mainContent.removeAttribute("inert");
+//   });
+
+// function openModal(modalId) {}
+// if (history.scrollRestoration) {
+//   history.scrollRestoration = "manual";
+// }
+
+// Remove URL hash on page load to prevent automatic scrolling
+// window.addEventListener("load", () => {
+//   if (window.location.hash) {
+//     history.replaceState(
+//       "",
+//       document.title,
+//       window.location.pathname + window.location.search,
+//     );
+//   }
+//   window.scrollTo(0, 0);
+// });
